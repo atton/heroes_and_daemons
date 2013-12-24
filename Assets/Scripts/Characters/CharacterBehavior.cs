354b93c61653136456911ab63dcdacfc24283fce
atton <e115763@gmail.com>
2013-12-24 22:22:32 +0900

    Control position parameter by field size

--- Assets/Scripts/Characters/CharacterBehavior.cs
+++ Assets/Scripts/Characters/CharacterBehavior.cs
@@ -7,8 +7,28 @@ public class CharacterBehavior : MonoBehaviour, IDamage {
 
 	protected CharacterStateMachine state;
 
+	/* TODO : this parameters split into character status class */
 	public    int MaxHitPoint = 100;
 	protected int hitPoint;
+
+
+	/* TODO : this parameters split into field parameters class */
+	protected float xRange = 3.0f;
+	protected float zRange = 20.0f;
+
+
+	protected void Update() {
+		positionControl();
+	}
+
+	void positionControl() {
+		Vector3 pos = transform.position;
+
+		pos.x = Mathf.Clamp(pos.x, -xRange, xRange);
+		pos.z = Mathf.Clamp(pos.z, -zRange, zRange);
+		
+		transform.position = pos;
+	}
 	
 	public virtual void Damage(DamageInfo info) {
 		hitPoint -= info.DamageValue();
--- Assets/Scripts/Characters/SlayerBehavior.cs
+++ Assets/Scripts/Characters/SlayerBehavior.cs
@@ -24,6 +24,7 @@ public class SlayerBehavior : CharacterBehavior {
 		UpdateStateFromInput();
 		state.UpdateFrameCount();
 		ActionFromState();
+		base.Update();
 	}
 
 	void UpdateStateFromInput() {

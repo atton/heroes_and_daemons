d3a9b0c7518969909f5933c38e2a3af731eb8116
atton <e115763@gmail.com>
2014-01-07 22:26:47 +0900

    Create Directory Scripts/Skill

---
+++ Assets/Scripts/Skill/Slayer/PunchBehavior.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using CharacterInterface;
+using System.Collections;
+
+namespace Skill {
+	namespace Slayer { 
+
+		public class PunchBehavior : MonoBehaviour {
+
+			public GameObject hitEffect;
+			const float punchPower       = 10000.0f;
+			const int   punchDamageValue = 50;
+
+			// Use this for initialization
+			void Start () {
+
+			}
+
+			// Update is called once per frame
+			void Update () {
+				Destroy(gameObject);	// punch is 1 frame
+			}
+
+			void OnCollisionEnter(Collision collision) {
+				Vector3 punch_vector = (collision.transform.position - transform.position);
+				collision.gameObject.rigidbody.AddForce(punchPower * punch_vector);
+
+				DamageInfo info = new DamageInfo();
+				info.SetDamageValue(punchDamageValue);
+
+				MonoBehaviour[] behaviors =  collision.gameObject.GetComponents<MonoBehaviour>();
+				foreach (MonoBehaviour b in behaviors) {
+					IDamage i = b as IDamage;
+					if (i != null) 	i.Damage(info);
+				}
+
+				Instantiate(hitEffect, transform.position, Quaternion.identity);
+				Destroy(gameObject);
+			}
+		}
+
+	}
+}

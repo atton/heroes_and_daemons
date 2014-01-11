1c121fd20bdf83e72192386714dfca8d2f31c9e5
atton <e115763@gmail.com>
2014-01-11 09:37:31 +0900

    Create skill select menu window, but window only

--- Assets/Scripts/GameSystem/CharacterSetting.cs
+++ Assets/Scripts/GameSystem/CharacterSetting.cs
@@ -7,9 +7,11 @@ public class CharacterSetting : MonoBehaviour {
 
 	private Rect exitMenu;
 	private Rect characterSelectMenu;
+	private Rect skillSelectMenu;
 	
 	private const int exitMenuID            = 0;
 	private const int characterSelectMenuID = 1;
+	private const int skillSelectMenuID     = 2;
 
 	private const int kRectSpace  = 20;
 	private const int kRectHeight = 100;
@@ -20,6 +22,11 @@ public class CharacterSetting : MonoBehaviour {
 		/* top */
 		characterSelectMenu = new Rect(kRectSpace, kRectSpace,
 		                               Screen.width - kRectSpace*2, kRectHeight);
+
+		/* middle */
+		skillSelectMenu    = new Rect(kRectSpace, kRectSpace*3 + kRectHeight,
+		                              Screen.width - kRectSpace*2, kRectHeight);
+
 		/* bottom */
 		exitMenu            = new Rect(kRectSpace, Screen.height - (kRectSpace + kRectHeight),
 		                               Screen.width - kRectSpace*2, kRectHeight);
@@ -29,6 +36,8 @@ public class CharacterSetting : MonoBehaviour {
 		exitMenu            = GUILayout.Window(exitMenuID, exitMenu, makeExitMenu, "Back to Main Menu");
 		characterSelectMenu = GUILayout.Window(characterSelectMenuID, characterSelectMenu,
 		                                       makeCharacterSelectMenu, "Please Select Your Character");
+		skillSelectMenu     = GUILayout.Window(skillSelectMenuID, skillSelectMenu,
+		                                       makeSkilSelectMenu, "Please Select Your Character Skills");
 	}
 
 	/* methods for create menu */
@@ -40,8 +49,16 @@ public class CharacterSetting : MonoBehaviour {
 	}
 
 	void makeCharacterSelectMenu(int id) {
+		GUILayout.BeginHorizontal();
+
 		if (GUILayout.Button("Slayer")) GlobalSettings.Setting.Character = Character.Slayer;
 		if (GUILayout.Button("Golem"))  GlobalSettings.Setting.Character = Character.Golem;
+
+		GUILayout.EndHorizontal();
+	}
+
+	void makeSkilSelectMenu(int id) {
+		if (GUILayout.Button("skill")) throw new UnityException("skill select is unimplemented.");
 	}
 		
 }

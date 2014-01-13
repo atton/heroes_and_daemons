9c08a4e23d9283ac8aa25384c65a4c05fc4277a8
atton <e115763@gmail.com>
2014-01-13 16:20:23 +0900

    Support Golem in Network

--- Assets/Scripts/Characters/Parameters/GolemParameter.cs
+++ Assets/Scripts/Characters/Parameters/GolemParameter.cs
@@ -4,8 +4,8 @@ using CharacterInterface;
 public class GolemParameter : CharacterParameter {
 
 	public GolemParameter() {
-		hitPoint           = MaxHitPoint;
 		maxHitPoint        = 200;
+		hitPoint           = MaxHitPoint;
 		runSpeed           = 50.0f;
 		CoolDownFrameShoot = 300;
 		CoolDownFrameMelee = 450;
